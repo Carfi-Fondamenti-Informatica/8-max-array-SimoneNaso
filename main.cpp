178b5a296eac6bf86d719adb6f5efe72abedf1a3
SimoneNaso <91719030+SimoneNaso@users.noreply.github.com>
2021-12-11 15:13:19 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
-#include <iostream>
 
-int main(){
-  return 0;
+#include <iostream>
+#include "lib.h"
+using namespace std;
+
+int main() {
+    int n=0;
+    cin >> n;
+    float A[n];
+        for (int i=0; i<n; i++){
+            cin >> A[i];
+     }
+    float risultato = max(A, n);
+    cout << risultato << endl;
+    return 0;
 }
