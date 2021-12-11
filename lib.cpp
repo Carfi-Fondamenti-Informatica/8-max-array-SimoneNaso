ef0da34cc3f7f3e3e6671d6bcc1c91901db1284d
SimoneNaso <91719030+SimoneNaso@users.noreply.github.com>
2021-12-11 15:12:21 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
 
+#include "lib.h"
+using namespace std;
+
+float max (const float A[], int dimensione){
+    float massimo=A[0];
+    for (int i=1; i<dimensione; i++) {
+        if (A[i]>massimo) {
+            massimo = A[i];
+        }
+    }
+    return massimo;
+}
